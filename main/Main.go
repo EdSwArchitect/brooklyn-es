40c2b0f084a3b6f99af2a3910dbd790f6f53f9bb
Ed Brown <edbrown@darkstar.dev>
2018-12-18 21:54:36 -0500

    Repackage

--- main/Main.go
+++ main/Main.go
@@ -1,80 +1,15 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io"
-	"log"
-	"strings"
 
 	"github.com/EdSwArchitect/brooklyn-es/query"
 )
 
-// Source for testing
-type Source struct {
-	TowardsBrooklyn  int32   `json:"towards_brooklyn"`
-	Location         string  `json:"location"`
-	Lat              float64 `json:"lat"`
-	TowardsManhattan int32   `json:"towards_manhattan"`
-	Timestamp        string  `json:"@timestamp"`
-	WeatherSummary   string  `json:"weather_summary"`
-	Temperature      int32   `json:"temperature"`
-	Events           string  `json:"events"`
-	Pedestriants     int32   `json:"Pedestrians"`
-	Lon              float64 `json:"lon"`
-	Precipitation    float64 `json:"precipitation"`
-}
-
-// Shards stuff
-type Shards struct {
-	Total      int32 `json:"total"`
-	Successful int32 `json:"successful"`
-	Skipped    int32 `json:"skipped"`
-	Failed     int32 `json:"failed"`
-}
-
-// BigHits hits
-type BigHits struct {
-	Total    int32   `json:"total"`
-	MaxScore float32 `json:"max_score"`
-	Hits     []Hits  `json:"hits"`
-}
-
-// Hits type
-type Hits struct {
-	Index   string  `json:"_index"`
-	DocType string  `json:"_type"`
-	Id      string  `json:"_id"`
-	Score   float32 `json:"_score"`
-	Source  Source  `json:"_source"`
-}
-
-// ESInfo stuff
-type ESInfo struct {
-	Took     int32   `json:"took"`
-	TimedOut bool    `json:"timed_out"`
-	Shards   Shards  `json:"_shards"`
-	Hits     BigHits `json:"hits"`
-}
-
 func main() {
 	fmt.Println("Hi, Ed")
 
-	contents := query.ReadJSON("/home/edbrown/Documents/brooklyn_results.json")
-
-	fmt.Println(contents)
-
-	dec := json.NewDecoder(strings.NewReader(contents))
-
-	for {
-		var esp ESInfo
-
-		if err := dec.Decode(&esp); err == io.EOF {
-			fmt.Println("EOF")
-			break
-		} else if err != nil {
-			log.Fatal(err)
-		}
+	if esp, err := query.ReadJSONObject("/home/edbrown/Documents/brooklyn_results.json"); err == nil {
 
 		fmt.Printf("Took - %d: TimedOut - %t \n", esp.Took, esp.TimedOut)
 		fmt.Printf("Shards.total: %d Shards.successful: %d Shards.skipped: %d Shards.failed :%d\n",
@@ -89,6 +24,8 @@ func main() {
 				dahit.Source.Location, dahit.Source.Lat,
 				dahit.Source.Lon, dahit.Source.WeatherSummary)
 		}
+	} else {
+		fmt.Println("Parse failed")
 	}
 
 }
--- query/JsonReader.go
+++ query/JsonReader.go
@@ -1,9 +1,59 @@
 package query
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
+	"strings"
 )
+
+// Source for testing
+type Source struct {
+	TowardsBrooklyn  int32   `json:"towards_brooklyn"`
+	Location         string  `json:"location"`
+	Lat              float64 `json:"lat"`
+	TowardsManhattan int32   `json:"towards_manhattan"`
+	Timestamp        string  `json:"@timestamp"`
+	WeatherSummary   string  `json:"weather_summary"`
+	Temperature      int32   `json:"temperature"`
+	Events           string  `json:"events"`
+	Pedestriants     int32   `json:"Pedestrians"`
+	Lon              float64 `json:"lon"`
+	Precipitation    float64 `json:"precipitation"`
+}
+
+// Shards stuff
+type Shards struct {
+	Total      int32 `json:"total"`
+	Successful int32 `json:"successful"`
+	Skipped    int32 `json:"skipped"`
+	Failed     int32 `json:"failed"`
+}
+
+// BigHits hits
+type BigHits struct {
+	Total    int32   `json:"total"`
+	MaxScore float32 `json:"max_score"`
+	Hits     []Hits  `json:"hits"`
+}
+
+// Hits type
+type Hits struct {
+	Index   string  `json:"_index"`
+	DocType string  `json:"_type"`
+	Id      string  `json:"_id"`
+	Score   float32 `json:"_score"`
+	Source  Source  `json:"_source"`
+}
+
+// ESInfo stuff
+type ESInfo struct {
+	Took     int32   `json:"took"`
+	TimedOut bool    `json:"timed_out"`
+	Shards   Shards  `json:"_shards"`
+	Hits     BigHits `json:"hits"`
+}
 
 // ReadJSON read the json file
 func ReadJSON(name string) string {
@@ -21,4 +71,20 @@ func ReadJSON(name string) string {
 
 	return json
 
+}
+
+// ReadJSONObject read the json string and return the object
+func ReadJSONObject(name string) (esp ESInfo, err error) {
+	contents := ReadJSON(name)
+
+	// var esp ESInfo
+
+	dec := json.NewDecoder(strings.NewReader(contents))
+
+	if err := dec.Decode(&esp); err != nil {
+		log.Fatal(err)
+		return esp, err
+	}
+
+	return esp, nil
 }
